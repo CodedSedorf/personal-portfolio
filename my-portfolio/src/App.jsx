2b8b1b3696c6e67beaebc20956a1b00d35b0e782
CodedSedorf <sedorfdlc@gmail.com>
2024-03-20 05:17:31 +0100

    navbar

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 // import { useState } from 'react'
 import './App.css'
+import MyNavBar from './component/MyNavBar'
 
 function App() {
 
   return (
     <div className='App'>
+      <MyNavBar/>
       <h1 className='font-primary'>Hello world</h1>
     </div>
   )
--- my-portfolio/src/component/MyNavBar.jsx
+++ my-portfolio/src/component/MyNavBar.jsx
@@ -3,7 +3,26 @@ import "../component/MyNavBar.css"
 
 const MyNavBar = () => {
   return (
-    <div>MyNavBar</div>
+    <div className='n-wrapper'>
+        <div className="n-left">
+            <div className="n-name">Dev Sedorf</div>
+            <span>toggle</span>
+        </div>
+        <div className="n-right">
+            <div className='n-list'>
+                <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Exprience</li>
+                    <li>Portfolio</li>
+                    <li>Testimonial</li>
+                </ul>
+            </div>
+            <button className='button'>
+                Contact Me
+            </button>
+        </div>
+    </div>
   )
 }
 
